f953e1d9f0e5677d8fc88844f8c799d63f654527
przemekorn@gmail.com <przemekorn@gmail.com>
2018-03-19 17:43:29 +0100

    poprawione js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,21 @@
+ $("#btn").click(function () {
+     $("body").append('<div id="dane-programisty"></div>');
+ });
+ $("#btn").click(function () {
+     $.ajax({
+             type: "GET",
+             url: "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
+             data: {
+                 get_param: "value"
+             },
+             dataType: "json",
+             success: (function (dane) {
+                 $.each(dane, function (index, e) {
+                     $("#dane-programisty").append(index + ": " + e + "<br>");
 
+                 })
 
-$.ajax({
-    type: "GET",
-    url:"https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
-    data: {get_param: "value"},
-    dataType: "json",
-    success: function(){
-        $("#btn").click(function(dane){
-        $.each(dane, function(index, e){
-            $("#dane-programisty").append(index +": " + e)
-            
-        })
-        
-})
-}
-})
-        
-        
-        
-/*
-      var myJSON = JSON.parse(objJSON);
-        $("#btn").click(function(){
-        $("#dane-programisty").load("Nazwisko: " + myJSONJSON.nazwisko);  
-        
-        })
-    
-    }
-})
-})
-
-
-
-
-*/
-
-
-
-
-
-/*$(document).ready(function () {
-    var objJSON = {
-        "imie": "Piotr",
-        "nazwisko": "Szapiel",
-        "zawod": "Programista",
-        "firma": "Akademia 108"
-    };
-    var myJSON = JSON.parse(objJSON);
-    $("#btn").click(function(){
-        $("#dane-programisty").load("Nazwisko: " + myJSONJSON.nazwisko);
-        
-        
-    })
-
-
-
-
-
-
-});*/
+             })
+         })
+     })
+ 
